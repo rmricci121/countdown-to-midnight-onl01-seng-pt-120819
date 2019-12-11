929f5162bcdbf17e871cfb62843e002254f3ac31
Robert M Ricci <rmricci121@gmail.com>
2019-12-11 01:23:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,11 @@ def countdown(number)
     "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep
+  while number > 0 do 
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+    sleep
+  end
+    "HAPPY NEW YEAR!"
+end
