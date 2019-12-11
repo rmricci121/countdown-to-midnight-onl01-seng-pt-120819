81d37f1e112ddbbad1cb5aa61d53cfd115aed157
Robert M Ricci <rmricci121@gmail.com>
2019-12-11 01:22:05 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,9 @@
-#write your code here
-require 'pry'
+
 def countdown(number)
   number 
   while number >= 0 do 
     puts "#{number} SECOND(S)!"
     number -= 1 
-    binding.pry
   end
     "HAPPY NEW YEAR!"
 end
